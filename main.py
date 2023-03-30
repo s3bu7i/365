e7b3e8e7a3566ea044c4e3412454932cb8c3dbe3
Sabuhi Gasimzada <96132884+s3bu7i@users.noreply.github.com>
2023-03-31 01:39:14 +0400

    0

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 
 # Define the base folder name
-base_folder = "2023.03."
+base_folder = "2023.04."
 
 # Define the number of folders to create
-num_folders = 31
+num_folders = 30
 
 # Loop through the number of folders to create
 for i in range(num_folders):
