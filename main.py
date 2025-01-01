13a67771f9db21b250525665f10a0ea64105fdde
Sabuhi Gasimzada <sabuhi.qasimzada2021@khazar.org>
2025-01-01 19:34:00 +0400

    *

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 
-base_folder = "2024.12."
+base_folder = "2025.1."
 
 num_folders = 31
 
 for i in range(num_folders):
-    
+
     folder_name = f'{base_folder}{i+1}'
     # Create the folder if it does not already exist
     if not os.path.exists(folder_name):
