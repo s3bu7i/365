2f3678afd4fab2daf4dc1b5503d85b8290e93990
Sabuhi Gasimzada <96132884+s3bu7i@users.noreply.github.com>
2025-04-21 23:34:17 +0400

    Update auto_commit.sh

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# Repo-nun yerli yolunu daxil edin
-REPO_PATH="https://github.com/s3bu7i/365"
-cd $REPO_PATH
+set -e
 
 # Faylı yenilə (random mətn əlavə et)
 echo "Update: $(date)" >> updates.txt
@@ -11,3 +8,4 @@ echo "Update: $(date)" >> updates.txt
 git add updates.txt
 git commit -m "Auto commit at $(date)"
 git push origin main
+
